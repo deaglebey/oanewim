bfb28a7f6cf6fa0ce40838105f79edd6c5680525
PeniaLife <36236673+PeniaLife@users.noreply.github.com>
2018-04-03 22:22:36 +0300

    Update cayic.js

--- komutlar/cayic.js
+++ komutlar/cayic.js
@@ -2,7 +2,7 @@ const ballAnswers = require('../cayic.json');
 var Discord =  require("discord.js");
 exports.run = function(client, message, args){
   const cay =new  Discord.RichEmbed()
-	  .setAuthor("Al Bakalım Benden Sana Çay." "" + message.author.username +   ")
+	  .setAuthor("Al Bakalım Benden Sana Çay" + message.author.username +  ")
 	.setColor("RANDOM")
   .setImage(ballAnswers[Math.floor(Math.random() * 2) +1])
   return message.channel.send(cay);
