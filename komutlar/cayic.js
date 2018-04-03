5da66b80432aee8eb9041cea0f7f861ff904356f
PeniaLife <36236673+PeniaLife@users.noreply.github.com>
2018-04-03 22:03:55 +0300

    Create cayic.js

---
+++ komutlar/cayic.js
@@ -0,0 +1,23 @@
+const ballAnswers = require('../mlbb.json');
+var Discord =  require("discord.js");
+exports.run = function(client, message, args){
+  const Enes =new  Discord.RichEmbed()
+  .setAuthor(message.author.username,"Al reis benden sana çay."
+	.setColor("RANDOM")
+  .setImage(ballAnswers[Math.floor(Math.random() * 2) +1])
+  return message.channel.send(Enes);
+  
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'çayiç',
+  description: 'Çay içmenize yarar.!',
+  usage: 'çayiç'
+};
