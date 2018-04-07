1723eade4d0619ff48b4b3e817ba7ff1c351236e
PeniaLife <36236673+PeniaLife@users.noreply.github.com>
2018-04-07 13:34:30 +0300

    Update istatistik.js

--- komutlar/istatistik.js
+++ komutlar/istatistik.js
@@ -8,7 +8,7 @@ exports.run = (client, message, params) => {
         
     .setColor('RANDOM')
     
-    .addField("Bot Adı","OA Premium",true),
+    .addField("Bot Adı","OA Premium",true)
     .addField("Total Kullanıcı",client.users.size,true)
 	    .addField("Ram Kullanımı",process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)+" MB **Kullanılıyor...**",true)
     
