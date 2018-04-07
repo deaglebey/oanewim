df857920e0aa17eeeee56a0bd8b446e97e57b18e
PeniaLife <36236673+PeniaLife@users.noreply.github.com>
2018-04-07 12:35:30 +0300

    Update reboot.js

--- komutlar/reboot.js
+++ komutlar/reboot.js
@@ -2,9 +2,6 @@ const Discord = require('discord.js');
 const moment = require('moment');
 
 exports.run = (client, message, args) =>
- if(message.author.id !=="327050092957794316"){
- message.channel.send(":x: Üzgünüm Bu Komut Yapımcım Tek Kullanabilir.!" + message.author)
- }
 {
 message.channel.sendMessage('Botun yeniden başlatılmasına onay veriyorsanız 30 saniye içinde evet yazın.')
 .then(() => {
