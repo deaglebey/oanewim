6e972804af5f9010ac67fc940e654764df89e84a
PeniaLife <36236673+PeniaLife@users.noreply.github.com>
2018-04-07 12:34:44 +0300

    Update sahip.js

--- komutlar/sahip.js
+++ komutlar/sahip.js
@@ -1,10 +1,14 @@
-onst Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 exports.run = (client, message, args) => {
  if(message.author.id !=="327050092957794316"){
  message.channel.send(":x: Üzgünüm Bu Komut Yapımcım Tek Kullanabilir.!" + message.author)
  }
+  
+
+ 
+ 
 };
 
 exports.conf = {
