34d32dacb84cc0f66a2e2c7fa0a09398746644d2
PeniaLife <36236673+PeniaLife@users.noreply.github.com>
2018-04-07 13:36:54 +0300

    Create istatistik.js

---
+++ komutlar/istatistik.js
@@ -0,0 +1,28 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+exports.run = (client, message, params) => {
+
+    const istatistik = new Discord.RichEmbed()
+		
+        
+    .setColor('RANDOM')
+    
+    .addField("Bot Adı","OA Premium")
+    .addField("Total Kullanıcı",client.users.size)
+	    .addField("Ram Kullanımı",process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)+" MB **Kullanılıyor...**")
+    
+    
+	
+    
+   
+    
+        
+  
+  
+          
+         
+        return message.channel.sendEmbed(istatistik);
+
+  
+  }
