7c37c536cc434d83c0554b31b54f74a2c77fcfb4
PeniaLife <36236673+PeniaLife@users.noreply.github.com>
2018-04-07 12:45:59 +0300

    Update sahip.js

--- komutlar/sahip.js
+++ komutlar/sahip.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 exports.run = (client, message, args) => {
  if(message.author.id !=="327050092957794316"){
- message.channel.send(":x: Üzgünüm Bu Komut Yapımcım Tek Kullanabilir.!" + message.author)
+ message.channel.send(":x: Üzgünüm Bu Komut Yapımcım Tek Kullanabilir.!")
  }
   
 message.channel.send("Yakında...!");
