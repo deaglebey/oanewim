14a625da23682bda23d8527f57195c91eb1c0555
PeniaLife <36236673+PeniaLife@users.noreply.github.com>
2018-04-07 12:31:37 +0300

    Create sahip.js

---
+++ komutlar/sahip.js
@@ -0,0 +1,21 @@
+onst Discord = require('discord.js');
+const client = new Discord.Client();
+
+exports.run = (client, message, args) => {
+ if(message.author.id !=="327050092957794316"){
+ message.channel.send(":x: Üzgünüm Bu Komut Yapımcım Tek Kullanabilir.!" + message.author)
+ }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [],
+  permLevel: 4
+};
+
+exports.help = {
+  name: 'sahipcmd',
+  description: 'Bot sahibinin komutlarını gösterir.!',
+  usage: 'sahipcmd'
+};
