fe15945b636cb0262892869fae5a8e92ed2e5d64
PeniaLife <36236673+PeniaLife@users.noreply.github.com>
2018-04-11 15:33:29 +0300

    Update temizle.js

--- komutlar/temizle.js
+++ komutlar/temizle.js
@@ -1,24 +1,35 @@
+const Discord = require('discord.js');
 exports.run = function(client, message, args) {
-  
-  
-  let messagecount = parseInt(args.join(' ')) + 1;
+   let messagecount = parseInt(args.join(' ')) + 1;
  
-  
+   if(isNaN(args[0])){
+        
+        message.reply(":no_entry: **Geçersiz sayı girdiniz,Tekrar deneyin.**" )
+    }
+    
+    else
+        {
+    
   message.channel.fetchMessages({
     limit: messagecount
   }).then(messages => message.channel.bulkDelete(messages));
   message.reply(":white_check_mark: Belirlenen miktarda mesaj sildim...").then(message => { setTimeout(function(){ message.delete(0); }, 3000); })
+        }
 };
+
+
+
 
 exports.conf = {
   enabled: true,
-  guildOnly: false,
+  guildOnly: true,
   aliases: [],
   permLevel: 2
 };
 
 exports.help = {
   name: 'temizle',
-  description: 'Belirlenen miktarda mesaj siler.',
-  usage: 'temizle <miktar>'
+  description: 'Belirlenen miktar mesajı siler.',
+  usage: 'temizle <temizlenecek mesaj sayısı>'
 };
+
