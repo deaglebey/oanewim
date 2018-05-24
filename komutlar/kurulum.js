6644d3cbc878e79768e5eff79f7028c2d0e47b3a
PeniaLife <36236673+PeniaLife@users.noreply.github.com>
2018-05-24 15:47:52 +0300

    Update kurulum.js

--- komutlar/kurulum.js
+++ komutlar/kurulum.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 
 exports.run = (client, message, args) => {
-    message.channel.send(new Discord.RichEmbed().setColor('RANDOM').setTitle('Komut giriişi').setDescription('Kurulıumları kabul ediyormusunuz.').setFooter('Bu eylemi onaylıyorsan "Evet" yazman yeterlidir.Bu eylem 30 saniye içinde sona erecek'))
+    message.channel.send(new Discord.RichEmbed().setColor('RANDOM').setTitle('Komut giriişi').setDescription('Kurulumları kabul ediyormusunuz.').setFooter('Bu eylemi onaylıyorsan "Evet" yazman yeterlidir.Bu eylem 30 saniye içinde sona erecek'))
 .then(() => {
 message.channel.awaitMessages(response => response.content === 'Evet', {
 max: 1,
@@ -10,9 +10,9 @@ time: 30000,
 errors: ['time'],
 })
 .then((collected) => {
-        message.guild.createChannel(`giriş-çıkış`);
-        message.guild.createChannel(`çağrılar`);
-        message.guild.createChannel(`mod-log`);
+        message.guild.createChannel(`öneri-komut`);
+        message.guild.createChannel(`logs`);
+        message.guild.createChannel(`yonetim-sohbet⚠`);
 
         message.channel.send(`Gerekli kanallar başarılı bir şekilde oluşturuldu.`);
     });
