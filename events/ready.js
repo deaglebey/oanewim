64bcd627a8b0d45d3d388b70dc2d66635d91d1d9
PeniaLife <36236673+PeniaLife@users.noreply.github.com>
2018-05-24 15:20:08 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,15 +2,37 @@ const chalk = require('chalk');
 const moment = require('moment');
 const Discord = require('discord.js');
 const ayarlar = require('../ayarlar.json');
+const snekfetch = require('snekfetch');
 
 var prefix = ayarlar.prefix;
 
 module.exports = client => {
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
-  client.user.setStatus("dnd");
-  client.user.setGame(`${prefix}yardım yazarak komutlarımı görebilirsin...|+${client.users.size} kullanıcıya hizmet veriliyor.!`)
-  //client.user.setGame(`${prefix}yardım + ${client.guilds.size} sunucu + ${client.users.size} kullanıcı`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+	
+console.log('>>Oynuyor kısmı başarıyla güncellendi.');
+console.log('Bot hazır ve giriş yaptı.');
+
+    var Games = [
+
+        "Yeni komut: oa-slot ✨",
+    
+        "Arkadaşlarını davet et:https://discord.gg/Dg83FZf  ✨",
+		
+		`${client.users.size} kullanıcı ✨`,
+		
+		`${prefix}yardım ✨`,
+		
+		
+	    
+	    	
+
+
+    ];
+
+    setInterval(function() {
+
+        var random = Math.floor(Math.random()*(Games.length-0+1)+0);
+
+        client.user.setGame(Games[random], "https://www.twitch.tv/batumurt11");
+        }, 2 * 2500);
+
 };
